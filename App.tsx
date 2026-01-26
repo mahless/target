4ccb040dee4e227e25483c81f5219776fdae9c2f
Mohamed Mahlis <mohamedmahlis@Mohameds-MacBook-Pro.local>
2026-01-26 05:09:34 +0200

    Cleaned up App.tsx Type Errors and Refined Walkthrough

--- App.tsx
+++ App.tsx
@@ -122,12 +122,16 @@ interface ErrorBoundaryState {
 }
 
 class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  public state: ErrorBoundaryState;
+  public props: ErrorBoundaryProps;
+
   constructor(props: ErrorBoundaryProps) {
     super(props);
     this.state = { hasError: false, error: null };
+    this.props = props;
   }
 
-  static getDerivedStateFromError(error: any) {
+  static getDerivedStateFromError(error: any): ErrorBoundaryState {
     return { hasError: true, error };
   }
 
